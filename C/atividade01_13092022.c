6ee633062c9fa4725aecbc3e52a717197b29dc6e
Pedro pwdrc Tolvo <pwdrc@protonmail.com>
2022-08-25 17:35:17 -0300

    Atividade 01 - BSI - 2022

---
+++ C/atividade01_13092022.c
@@ -0,0 +1,32 @@
+/*
+
+  	Instruções:
+
+	Atividade:
+
+	Faça um programa em C que receba o nome do método de ordenação (no caso, selecao ou bolha), o número N de elementos de um vetor. 
+	Após isso, leia os N elementos e imprima as indicações de todas as comparações e trocas realizadas pelo método escolhido, seguido do vetor ordenado.
+
+	As comparações e trocas devem ser impressas de acordo com os índices dos elementos envolvidos na operação. 
+	Por exemplo, se o elemento do índice 3 foi comparado com o elemento do índice 7, então deve-se imprimir C 3 7. 
+	Se o elemento de índice 2 foi trocado com o elemento de índice 5, deve-se imprimir T 2 5. 
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	
+	char metodo[7];
+	scanf("%s", metodo);
+	//printf("%s", metodo);
+	
+	if(strcmp(metodo, "selecao") == 0)
+		printf("selecao");
+
+	else if(strcmp(metodo, "bolha") == 0)
+		printf("bolha");
+
+	else printf("nada");
+}
+
