d21420558775709e6c725f3cb5da51cd08ba7d89
Pedro Tolvo <pwdrc@protonmail.com>
2022-09-27 11:23:09 -0300

    random java print

--- java/basic/RandomWord.java
+++ java/basic/RandomWord.java
@@ -5,10 +5,11 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
     public static void main(String[] args) {
         int i = 1;
+        String FinalWord = "string";
         do {
           String a = StdIn.readString();
           if(StdRandom.bernoulli(i)) {
-            String FinalWord = a;
+            FinalWord = a;
           }
           i++;
         } while (!(StdIn.isEmpty()));
