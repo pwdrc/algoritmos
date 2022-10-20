17f225adcd331425e3c7cb7a6c85ea2331e23570
Pedro Tolvo <pwdrc@protonmail.com>
2022-10-20 10:30:19 -0300

    java methods parametros

---
+++ java/basic/w3schools/methods/parametros.java
@@ -0,0 +1,12 @@
+public class parametros {
+
+	static void imprimeNome(String nome) {
+		System.out.println("Imprime nome: " + nome);
+	}
+
+	public static void main (String[] args) {
+		imprimeNome("Pedro");
+		imprimeNome("Tiago");
+		imprimeNome("nome");
+	}
+}
